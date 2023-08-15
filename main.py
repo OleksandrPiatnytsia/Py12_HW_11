80a311b4f416f2ff247847953ed572924abec5d9
Oleksandr <115171014+OleksandrPiatnytsia@users.noreply.github.com>
2023-08-15 23:20:13 +0300

    routes changed

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ def read_root():
 
 
 @app.get("/api/healthchecker")
-def healthchecker(db: Session = Depends(get_db)):
+def healthchecker(session: Session = Depends(get_db)):
     try:
         # Make request
-        result = db.execute(text("SELECT 1")).fetchone()
+        result = session.execute(text("SELECT 1")).fetchone()
         if result is None:
             raise HTTPException(status_code=500, detail="Database is not configured correctly")
         return {"message": "Welcome to FastAPI!"}
@@ -32,13 +32,13 @@ def healthchecker(db: Session = Depends(get_db)):
         raise HTTPException(status_code=500, detail="Error connecting to the database")
 
 
-@app.get("/contacts", response_model=List[ContactSchema], tags=["contacts"])
-def get_owners(db: Session = Depends(get_db)):
-    contacts = db.query(Contact).all()
+@app.get("/api/contacts", response_model=List[ContactSchema], tags=["contacts"])
+def get_owners(session: Session = Depends(get_db)):
+    contacts = session.query(Contact).all()
     return contacts
 
 
-@app.get("/contacts/{contact_id}", response_model=ContactSchema, tags=["contacts"])
+@app.get("/api/contacts/{contact_id}", response_model=ContactSchema, tags=["contacts"])
 async def get_owner(contact_id: int = Path(ge=1), db: Session = Depends(get_db)):
     contact = db.query(Contact).filter_by(id=contact_id).first()
     if contact is None:
@@ -49,9 +49,9 @@ async def get_owner(contact_id: int = Path(ge=1), db: Session = Depends(get_db))
     return contact
 
 
-@app.post("/contacts", response_model=ContactSchema, tags=["contacts"])
-async def create_owner(body: ContactSchema, db: Session = Depends(get_db)):
-    contact = db.query(Contact).filter_by(phone=body.phone).first()
+@app.post("/api/contacts", response_model=ContactSchema, tags=["contacts"])
+async def create_owner(body: ContactSchema, session: Session = Depends(get_db)):
+    contact = session.query(Contact).filter_by(phone=body.phone).first()
     if contact:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
@@ -63,6 +63,6 @@ async def create_owner(body: ContactSchema, db: Session = Depends(get_db)):
     contact.name = body.name
     contact.sur_name = body.sur_name
     contact.birthday = body.birthday
-    db.add(contact)
-    db.commit()
+    session.add(contact)
+    session.commit()
     return contact
